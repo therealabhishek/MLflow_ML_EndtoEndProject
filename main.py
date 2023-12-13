129596d6a328b49625e37481e1f0fe940d4ad683
Abhishek Kulkarni <kul.abhishake@gmail.com>
2023-12-13 09:51:41 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.mlflow_Project import logger
 
-logger.info("Welcome to our custom logger.")
+from src.mlflow_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
